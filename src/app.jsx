62ee92cf7cac2700f9055d10038e617b179a702c
rllola <rllola80@gmail.com>
2018-01-16 16:53:07 +0100

    First commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
+
+class App extends Component {
+  render() {
+    return (<div>
+      <h2>Welcome to Lola!</h2>
+    </div>)
+  }
+}
+
+export default hot(module)(App)
