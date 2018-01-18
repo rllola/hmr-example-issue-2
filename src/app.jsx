2785016249ab5c71e1db600135cbc70a83a429d7
rllola <rllola80@gmail.com>
2018-01-18 11:50:55 +0100

    Testing  with HMR v3

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import { hot } from 'react-hot-loader'
 
 class App extends Component {
   render() {
     return (<div>
-      <h2>Welcome to Lola!</h2>
+      <h2>Welcome to Guillaume!</h2>
     </div>)
   }
 }
 
-export default hot(module)(App)
+export default App
